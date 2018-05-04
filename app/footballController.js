f50c6170ab6001848876021f576dcbc4f0b66cbb
Name <username@email.com>
2018-05-04 10:09:19 -0600

    First add

---
+++ app/footballController.js
@@ -0,0 +1,15 @@
+function FootballController()
+
+//PRIVATE
+
+var footballService = new footballService(drawRoster);
+
+function drawRoster (players){
+    var template = "<h1>Player Roster</h1>";
+        for (let i = 0; i < players.length; i++) {
+            const player = players[i];
+            template =+``;
+            
+        }
+    
+}
